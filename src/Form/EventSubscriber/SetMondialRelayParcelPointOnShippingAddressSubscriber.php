0a11e46e2093d8db4e1fce521c7944288fe528be
Maxime Veber <nek.dev@gmail.com>
2023-04-11 17:49:46 +0200

    feat: re-insert previous address
    Problem: if the user change back to another delivery, the address filled
    is the one of the pickup point. To solve this I suggest to store the old
    address in the session and taking it back on change for another delivery

--- src/Form/EventSubscriber/SetMondialRelayParcelPointOnShippingAddressSubscriber.php
+++ src/Form/EventSubscriber/SetMondialRelayParcelPointOnShippingAddressSubscriber.php
@@ -8,11 +8,15 @@ use Sylius\Component\Core\Model\ShipmentInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Wishibam\SyliusMondialRelayPlugin\DependencyInjection\ParsedConfiguration;
 use Wishibam\SyliusMondialRelayPlugin\Form\Extension\ShippingMethodChoiceTypeExtension;
 
 class SetMondialRelayParcelPointOnShippingAddressSubscriber implements EventSubscriberInterface
 {
+    public function __construct(private SessionInterface $session)
+    {
+    }
     public static function getSubscribedEvents()
     {
         return [
@@ -25,24 +29,55 @@ class SetMondialRelayParcelPointOnShippingAddressSubscriber implements EventSubs
         $shipment = $event->getData();
         $form = $event->getForm();
 
+        /** @var Address $originalShippingAddress */
+        $originalShippingAddress = $shipment->getOrder()->getShippingAddress();
+
         if (
             !$shipment instanceof ShipmentInterface ||
             null === $shipment->getMethod() ||
             null === $shipment->getOrder() ||
             ParsedConfiguration::MONDIAL_RELAY_CODE !== $shipment->getMethod()->getCode()
         ) {
+            $this->resetAddress($originalShippingAddress);
             return;
         }
 
         /** @var Address $mondialRelayPointAddress */
         $mondialRelayPointAddress = $form->get('mondialRelayParcelAddress')->getData();
-        /** @var Address $originalShippingAddress */
-        $originalShippingAddress = $shipment->getOrder()->getShippingAddress();
+        $this->saveAddressData($originalShippingAddress);
+
         // Replace the original shipping address info by the mondial relay parcel point info
         $originalShippingAddress->setPostcode($mondialRelayPointAddress->getPostcode());
         $originalShippingAddress->setStreet($mondialRelayPointAddress->getStreet());
         $originalShippingAddress->setCity($mondialRelayPointAddress->getCity());
         // Example "Amazing shop name---FR-008046"
         $originalShippingAddress->setCompany($mondialRelayPointAddress->getCompany() . ShippingMethodChoiceTypeExtension::SEPARATOR_PARCEL_NAME_AND_PARCEL_ID.$form->get('parcelPoint')->getData());
+    }
+
+    private function saveAddressData(Address $address)
+    {
+        $this->session->set('mondialRelayPreviousAddress', [
+            'postCode' => $address->getPostcode(),
+            'street' => $address->getStreet(),
+            'city' => $address->getCity(),
+            'company' => $address->getCompany()
+        ]);
+    }
+
+    private function resetAddress(Address $address)
+    {
+        /** @var null|array{postCode: ?string, street: ?string, city: ?string, company: ?string} $previousAddress */
+        $previousAddress = $this->session->get('mondialRelayPreviousAddress');
+
+        if (null === $previousAddress) {
+            return;
+        }
+
+        $address->setCity($previousAddress['city']);
+        $address->setPostcode($previousAddress['postCode']);
+        $address->setCompany($previousAddress['company']);
+        $address->setStreet($previousAddress['street']);
+
+        $this->session->set('mondialRelayPreviousAddress', null);
     }
 }
