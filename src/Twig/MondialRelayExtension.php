4c82fd4f83377fe8527fe552ccad5fa53d4ef959
Julien Bourdin <jbourdin@gmail.com>
2024-12-27 09:29:07 +0100

    Revert "Revert "feature: restrict widget to french configured browsers""

--- src/Twig/MondialRelayExtension.php
+++ src/Twig/MondialRelayExtension.php
@@ -10,22 +10,33 @@ use Twig\TwigFunction;
 use Wishibam\SyliusMondialRelayPlugin\Configuration\ConfigurationResolver;
 use Wishibam\SyliusMondialRelayPlugin\Configuration\ParsedConfiguration;
 use Wishibam\SyliusMondialRelayPlugin\Form\Extension\ShippingMethodTypeExtension;
+use Wishibam\SyliusMondialRelayPlugin\Service\RequestLocaleCheckerInterface;
 
 class MondialRelayExtension extends AbstractExtension
 {
     private ConfigurationResolver $configurationResolver;
+    private RequestLocaleCheckerInterface $requestLocaleChecker;
 
-    public function __construct(ConfigurationResolver $configurationResolver)
-    {
+    public function __construct(
+        ConfigurationResolver $configurationResolver,
+        RequestLocaleCheckerInterface $requestLocaleChecker
+    ) {
         $this->configurationResolver = $configurationResolver;
+        $this->requestLocaleChecker = $requestLocaleChecker;
     }
 
     public function getFunctions(): array
     {
         return [
+            new TwigFunction('isRequestLocaleAllowedForMondialRelay', [$this, 'isRequestLocaleAllowedForMondialRelay']),
             new TwigFunction('isMondialRelayShippingMethod', [$this, 'isMondialRelayShippingMethod']),
             new TwigFunction('getMondialRelayConfiguration', [$this, 'getMondialRelayConfiguration']),
         ];
+    }
+
+    public function isRequestLocaleAllowedForMondialRelay(): bool
+    {
+        return $this->requestLocaleChecker->isLocaleAllowed();
     }
 
     public function isMondialRelayShippingMethod(ShippingMethodInterface $shippingMethod): bool
